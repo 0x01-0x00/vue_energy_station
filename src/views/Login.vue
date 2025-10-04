2942e0d89ca217148afbc9706860bce5cff74d9a
zhongtao <ztappleemail@163.com>
2025-10-04 20:42:23 +0800

    feat(login): 实现登录页面UI布局和样式- 添加背景图片和登录框样式 - 实现logo和标题展示 - 添加用户名和密码输入框 - 添加登录按钮 - 使用Element Plus组件库构建表单 - 引入logo图片资源 - 设置页面居中布局和阴影效果

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,3 +1,60 @@
 <template>
-  login组件
+  <div class="bg">
+
+    <div class="login">
+      <div class="logo">
+        <img :src="log0Img" alt="" width="70px" height="70px">
+        <h1 class="ml">小华能源充电管理平台</h1>
+      </div>
+
+      <el-form>
+        <el-form-item>
+          <el-input prefix-icon="User" placeholder="用户名"/>
+        </el-form-item>
+        <el-form-item>
+          <el-input prefix-icon="Lock" placeholder="密码"/>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" style="width: 100%">登录</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+
+  </div>
 </template>
+
+<script setup lang="ts">
+import log0Img from "@/assets/logo.png"
+// import {User, Lock} from "@element-plus/icons-vue";
+
+</script>
+
+<style scoped lang="less">
+.bg{
+  background-image: url("../assets/bg.png");
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 100vh;
+
+  .login {
+    .logo{
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin-bottom: 40px;
+      h1{
+        color: rgb(14, 53, 148);
+      }
+    }
+    width: 500px;
+    height: 300px;
+    padding: 50px;
+    box-shadow: 0 0 10px 10px #f4f4f4;
+    text-align: center;
+    position: absolute;
+    top: 50%;
+    margin-top: -200px;
+  }
+}
+</style>
