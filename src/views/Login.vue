1510a4982aa5041631f4de1f9ea52dc2727a0ccb
zhongtao <ztappleemail@163.com>
2025-10-05 16:03:56 +0800

    feat(app): 集成 Pinia 状态管理和 Axios HTTP 客户端- 在 main.ts 中配置并注册 Pinia 状态管理库 - 添加 Axios 作为项目依赖用于发送 HTTP 请求 - 更新 package.json 和 lock 文件以包含新依赖项 - 自动安装 Axios 相关的嵌套依赖包

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -7,15 +7,15 @@
         <h1 class="ml">小华能源充电管理平台</h1>
       </div>
 
-      <el-form>
-        <el-form-item>
-          <el-input prefix-icon="User" placeholder="用户名"/>
+      <el-form :model="loginForm" :rules="rules">
+        <el-form-item prop="name">
+          <el-input prefix-icon="User" placeholder="用户名" v-model="loginForm.name"/>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input prefix-icon="Lock" placeholder="密码" v-model="loginForm.password" type="password"/>
         </el-form-item>
         <el-form-item>
-          <el-input prefix-icon="Lock" placeholder="密码"/>
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" style="width: 100%">登录</el-button>
+          <el-button type="primary" style="width: 100%" @click="handleLogin">登录</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -25,12 +25,45 @@
 
 <script setup lang="ts">
 import log0Img from "@/assets/logo.png"
-// import {User, Lock} from "@element-plus/icons-vue";
+import {reactive,ref} from "vue";
+import type {FormRules, FormInstance} from "element-plus"
+// 登录表单
+interface LoginForm {
+  name: string;
+  password: string;
+}
+// 表单验证
+const loginForm: LoginForm = reactive({
+  name: "",
+  password: "",
+})
+// 表单验证规则
+const rules=reactive<FormRules<LoginForm>>({
+  name: [
+    {required: true, message: "请输入用户名", trigger: "blur"},
+    {min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur"},
+  ],
+  password: [
+    {required: true, message: "请输入密码", trigger: "blur"},
+    {min: 3, max: 5, message: "长度在 3 到 5 个字符", trigger: "blur"},
+  ],
+})
+
+const formRef = ref<FormInstance>();
+const handleLogin = () => {
+  formRef.value?.validate(valid: boolean => {
+    if (valid) {
+      console.log("登录成功")
+    } else {
+      console.log("登录失败")
+    }
+  })
+}
 
 </script>
 
 <style scoped lang="less">
-.bg{
+.bg {
   background-image: url("../assets/bg.png");
   background-size: cover;
   background-position: center;
@@ -38,15 +71,17 @@ import log0Img from "@/assets/logo.png"
   height: 100vh;
 
   .login {
-    .logo{
+    .logo {
       display: flex;
       justify-content: center;
       align-items: center;
       margin-bottom: 40px;
-      h1{
+
+      h1 {
         color: rgb(14, 53, 148);
       }
     }
+
     width: 500px;
     height: 300px;
     padding: 50px;
